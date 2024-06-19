3d4043f5af03a6787a8faf9c9599ce646cb40dc6
Teodor Todorov <teodort@channelz.io>
2024-06-20 01:33:14 +0300

    scheduler days and some improvements;

--- src/components/schedule/schedule.tsx
+++ src/components/schedule/schedule.tsx
@@ -3,12 +3,23 @@ import styles from './schedule.module.css';
 import Arrow from '../../assets/arrow.svg?react';
 import classnames from 'classnames';
 
+const today = new Date().toLocaleDateString('en-CA');
+
 const Schedule = () => {
   const [startDate, setStartDate] = useState<string>('');
   const [endDate, setEndDate] = useState<string>('');
   const [dateRange, setDateRange] = useState<number>(0);
 
   const range = dateRange > 1 ? 'days' : 'day';
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
   const handleDateChange = (
     e: ChangeEvent<HTMLInputElement>,
@@ -31,7 +42,7 @@ const Schedule = () => {
     if (start && end) {
       const diffTime = Math.abs(endDate - startDate);
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      setDateRange(diffDays);
+      setDateRange(diffDays + 1);
     } else {
       setDateRange(0);
     }
@@ -51,6 +62,7 @@ const Schedule = () => {
             <input
               type='date'
               value={startDate}
+              min={today}
               max={endDate}
               onClick={handleOpenPicker}
               onChange={(e) => handleDateChange(e)}
@@ -66,6 +78,7 @@ const Schedule = () => {
               onClick={handleOpenPicker}
               onChange={(e) => handleDateChange(e, true)}
               className={styles.input}
+              disabled={!startDate}
             />
           </div>
 
@@ -85,6 +98,36 @@ const Schedule = () => {
             })}
           />
         </div>
+      </div>
+      <div className={styles.scheduler}>
+        {Array.from({ length: dateRange }, (_, i) => i + 1).map((c, index) => {
+          const currentDate = new Date(startDate);
+          currentDate.setDate(currentDate.getDate() + index);
+
+          const day = currentDate
+            .toLocaleDateString('en-ca')
+            .replace(/-/g, '.');
+
+          return (
+            <div className={styles.column}>
+              <div className={styles.day}>
+                <div className={styles.dTitle}>
+                  {dayNames[currentDate.getDay()]}
+                </div>
+                <div>{day}</div>
+              </div>
+              <div className={styles.hoursContainer}>
+                <div
+                  className={classnames(styles.hours, {
+                    // [styles.hrsDisabled]: 'disabled',
+                  })}
+                >
+                  9:00
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
