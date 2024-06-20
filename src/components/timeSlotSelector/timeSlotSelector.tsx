e0a9349d4306423e67cb2cfd8a2b635b5625cb6d
Teodor Todorov <teodort@channelz.io>
2024-06-20 16:41:09 +0300

    timeSlotSelector some test changes

--- src/components/timeSlotSelector/timeSlotSelector.tsx
+++ src/components/timeSlotSelector/timeSlotSelector.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './timeSlotSelector.module.css';
 import classnames from 'classnames';
 
@@ -10,13 +10,43 @@ interface TimeSlotSelectorProps {
 const TimeSlotSelector = (props: TimeSlotSelectorProps) => {
   const { schedulerRef, dateRange } = props;
 
-  //   const [dateTimes, setDateTimes] = useState([{}]);
+  const [dateTimes, setDateTimes] = useState(dateRange);
   const [isColumnHovered, setIsColumnHovered] = useState(-1);
+
+  useEffect(() => {
+    setDateTimes(dateRange);
+    console.log('UPDATED');
+  }, [dateRange]);
 
   return (
     <div className={styles.scheduler} ref={schedulerRef}>
-      {dateRange.map((c, index) => {
-        const { date, day } = c;
+      {dateTimes.map((c, index) => {
+        const { date, day, times } = c;
+
+        const handleAddTime = () => {
+          const updated = dateTimes.map((d) => {
+            if (d.date === date && times.length < 4) {
+              return { ...d, times: [...times, ''] };
+            }
+            return d;
+          });
+          setDateTimes(updated);
+        };
+
+        const getRelatedTime = () => {
+          switch (times.length) {
+            case 0:
+              return '9:00';
+            case 1:
+              return '12:00';
+            case 2:
+              return '16:00';
+            case 3:
+              return '20:00';
+            default:
+              return;
+          }
+        };
 
         const onColumnHover = () => {
           setIsColumnHovered(index);
@@ -36,16 +66,26 @@ const TimeSlotSelector = (props: TimeSlotSelectorProps) => {
               onMouseEnter={onColumnHover}
               onMouseLeave={onColumnHoverOut}
             >
-              <div
-                className={classnames(styles.hours, {
-                  // [styles.hrsDisabled]: 'disabled',
-                })}
-              >
-                9:00
-              </div>
+              {Array.from({ length: times.length + 1 }, (_, i) => i + 1).map(
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                (_, index) => {
+                  return (
+                    <div
+                      className={classnames(styles.hours, {
+                        // [styles.hrsDisabled]: 'disabled',
+                      })}
+                    >
+                      {getRelatedTime()}
+                    </div>
+                  );
+                }
+              )}
 
               {isColumnHovered === index && (
-                <div className={classnames(styles.hours, styles.addTime)}>
+                <div
+                  onClick={handleAddTime}
+                  className={classnames(styles.hours, styles.addTime)}
+                >
                   Add Time
                 </div>
               )}
